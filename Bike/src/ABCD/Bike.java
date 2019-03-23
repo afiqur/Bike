c3b42dac026bee51c6735253aa0493a2deacd738
Afiqur Rahman <afiqur@users.noreply.github.com>
2019-03-23 23:12:03 +0600

    First Commit

---
+++ Bike/src/ABCD/Bike.java
@@ -0,0 +1,18 @@
+package ABCD;
+
+public class Bike {
+
+	public static String name = "Walton";
+
+	public Bike() {
+		System.out.println("Hello Bike");
+	}
+
+	public static void main(String[] args) {
+		Bike b = new Bike();
+		System.out.println(b.name + " Bike");
+
+	}
+
+}
+
